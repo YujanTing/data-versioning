88f8743e4fd589dbf2ee01a0734122ae99f0dd26
YujanTing <yujanting@hms.harvard.edu>
2023-09-21 23:27:19 -0400

    dataset updates...

--- docker-shell.sh
+++ docker-shell.sh
@@ -4,7 +4,8 @@ set -e
 
 export BASE_DIR=$(pwd)
 export SECRETS_DIR=$(pwd)/../secrets/
-export GCS_BUCKET_NAME="mushroom-app-data-demo"
+
+export GCS_BUCKET_NAME="lec5_data_bucket"
 export GCP_PROJECT="ac215-project"
 export GCP_ZONE="us-central1-a"
 
